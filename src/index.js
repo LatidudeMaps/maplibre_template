e3b6cf0acd9059f37e58ca8171e01f5cfec341df
Latidude Maps <latidude.maps@gmail.com>
2025-03-05 17:41:39 +0100

    Aggiunto style.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
+import style from './src/style.json';
 
 document.addEventListener('DOMContentLoaded', () => {
     const map = new maplibregl.Map({
         container: 'map',
-        style: 'https://demotiles.maplibre.org/style.json', // Basemap gratuita di default
+        style: style,  // Usa lo stile locale invece dell'URL
         center: [12.4963, 41.9027], // Coordinate di Roma, cambia in base alle tue esigenze
         zoom: 5
     });
